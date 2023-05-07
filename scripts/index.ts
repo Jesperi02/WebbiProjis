c75837cec4de5eacb14ca89c161927ad0f59bf7f
Jere Niemi <jere.viljami.niemi@gmail.com>
2023-05-07 20:07:19 +0300

    Ajax haku toimii

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,7 +1,20 @@
-var news = document.getElementById("news");
+var consumerpriceindexTable = document.getElementById("consumerpriceindexTable");
 
-if (news) {
-	news.textContent = "Hei moi";	
+if (consumerpriceindexTable) {
+	getConsumerpriceIndex();
 } else {
-	console.error("news elementtiä ei löydetty!");
+	console.error("consumerpriceindexTable elementtiä ei löydetty!");
+}	
+
+async function getConsumerpriceIndex() {
+	const response = await fetch("https://pxdata.stat.fi:443/PxWeb/api/v1/fi/StatFin/khi/statfin_khi_pxt_11xb.px");
+	const jsonData = await response.json();
+	console.log(jsonData);
+
+	let head = consumerpriceindexTable.childNodes[0];
+	let body = consumerpriceindexTable.childNodes[1];
+
+	let trhead = document.createElement("tr");
+
+	//let th1
 }
